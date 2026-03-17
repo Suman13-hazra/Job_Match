230d9eaf7705e9a34cd1f57339d262a9894e6ce0
Suman13-hazra <sumanhazra0513@gmail.com>
2026-03-18 03:38:58 +0530

    Fixed deployment config

--- backend/app.py
+++ backend/app.py
@@ -789,4 +789,4 @@ def serve_static(path):
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0',port=int(os.environ.get("PORT", 10000)))
